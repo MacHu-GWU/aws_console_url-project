a8332e07efc2f0bb568ea7567b55f97b420db17e
Sanhe Hu <husanhe@gmail.com>
2023-02-02 22:53:01 -0500

    lambda

--- aws_console_url/srv/awslambda.py
+++ aws_console_url/srv/awslambda.py
@@ -3,11 +3,11 @@
 import typing as T
 import dataclasses
 
-from ..builder import ConsoleUrlBuilder, ArnBuilder
+from ..model import Service, Resource
 
 
 @dataclasses.dataclass(frozen=True)
-class LambdaFunctionArn(ArnBuilder):
+class LambdaFunction(Resource):
     name: T.Optional[str] = dataclasses.field(default=None)
     version: T.Optional[int] = dataclasses.field(default=None)
     alias: T.Optional[str] = dataclasses.field(default=None)
@@ -17,10 +17,10 @@ class LambdaFunctionArn(ArnBuilder):
         cls,
         aws_account_id: str,
         aws_region: str,
-        name,
+        name: str,
         version: T.Optional[int] = None,
         alias: T.Optional[str] = None,
-    ) -> "LambdaFunctionArn":
+    ) -> "LambdaFunction":
         return cls(
             aws_account_id=aws_account_id,
             aws_region=aws_region,
@@ -51,7 +51,7 @@ class LambdaFunctionArn(ArnBuilder):
         return arn
 
     @classmethod
-    def from_arn(cls, arn: str) -> "LambdaFunctionArn":
+    def from_arn(cls, arn: str) -> "LambdaFunction":
         parts = arn.split(":")
         aws_account_id = parts[4]
         aws_region = parts[3]
@@ -83,50 +83,109 @@ class LambdaFunctionArn(ArnBuilder):
 
 
 @dataclasses.dataclass(frozen=True)
-class AWSLambda(ConsoleUrlBuilder):
+class LambdaLayer(Resource):
+    name: T.Optional[str] = dataclasses.field(default=None)
+    version: T.Optional[int] = dataclasses.field(default=None)
+
+    @classmethod
+    def make(
+        cls,
+        aws_account_id: str,
+        aws_region: str,
+        name: str,
+        version: int,
+    ) -> "LambdaLayer":
+        return cls(
+            aws_account_id=aws_account_id,
+            aws_region=aws_region,
+            name=name,
+            version=version,
+        )
+
+    @property
+    def arn(self) -> str:
+        return f"arn:aws:lambda:{self.aws_region}:{self.aws_account_id}:layer:{self.name}:{self.version}"
+
+    @classmethod
+    def from_arn(cls, arn: str) -> "LambdaLayer":
+        parts = arn.split(":")
+        aws_account_id = parts[4]
+        aws_region = parts[3]
+        name = parts[6]
+        version = parts[7]
+        return cls.make(aws_account_id, aws_region, name, int(version))
+
+
+@dataclasses.dataclass(frozen=True)
+class AWSLambda(Service):
     _AWS_SERVICE = "lambda"
 
-    # --- Menu
+    # --- arn
+    def get_function_arn(
+        self,
+        name: str,
+        version: T.Optional[int] = None,
+        alias: T.Optional[str] = None,
+    ) -> str:
+        return LambdaFunction.make(
+            self._account_id, self._region, name, version, alias
+        ).arn
+
+    def get_layer_arn(
+        self,
+        name: str,
+        version: int,
+    ) -> str:
+        return LambdaLayer.make(self._account_id, self._region, name, version).arn
+
+    # --- dashboard
     @property
     def functions(self) -> str:
-        return f"{self._service_root}/home?#/functions"
+        return f"{self._service_root}/home?region={self._region}#/functions"
 
     def filter_functions(self, name: str) -> str:
-        return f"{self._service_root}/home?#/functions?fo=and&o0=%3A&v0={name}"
+        return f"{self._service_root}/home?region={self._region}#/functions?fo=and&o0=%3A&v0={name}"
 
     @property
     def layers(self) -> str:
-        return f"{self._service_root}/home?#/layers"
+        return f"{self._service_root}/home?region={self._region}#/layers"
 
-    # --- Function
-    def _function_tab(self, name: str, tab: str) -> str:
-        return f"{self._service_root}/home?#/functions/{name}?tab={tab}"
+    # --- lambda function
+    def _get_function_tab(self, name: str, tab: str) -> str:
+        return f"{self._service_root}/home?region={self._region}#/functions/{name}?tab={tab}"
 
     def get_function(self, name: str) -> str:
-        return self._function_tab(name, "code")
+        return self._get_function_tab(name, "code")
 
     def get_function_code_tab(self, name: str) -> str:
-        return self._function_tab(name, "code")
+        return self._get_function_tab(name, "code")
 
     def get_function_test_tab(self, name: str) -> str:
-        return self._function_tab(name, "testing")
+        return self._get_function_tab(name, "testing")
 
     def get_function_monitor_tab(self, name: str) -> str:
-        return self._function_tab(name, "monitoring")
+        return self._get_function_tab(name, "monitoring")
 
     def get_function_config_tab(self, name: str) -> str:
-        return self._function_tab(name, "configure")
+        return self._get_function_tab(name, "configure")
 
     def get_function_alias_tab(self, name: str) -> str:
-        return self._function_tab(name, "aliases")
+        return self._get_function_tab(name, "aliases")
 
     def get_function_version_tab(self, name: str) -> str:
-        return self._function_tab(name, "versions")
+        return self._get_function_tab(name, "versions")
 
     def get_function_version(self, name: str, version: int) -> str:
         return (
-            f"{self._service_root}/home?#/functions/{name}/versions/{version}?tab=code"
+            f"{self._service_root}/home?region={self._region}#/functions/{name}/versions/{version}?tab=code"
         )
 
     def get_function_alias(self, name: str, alias: str) -> str:
-        return f"{self._service_root}/home?#/functions/{name}/aliases/{alias}?tab=configure"
+        return f"{self._service_root}/home?region={self._region}#/functions/{name}/aliases/{alias}?tab=configure"
+
+    # --- layer
+    def get_layer(self, name: str, version: int=1) -> str:
+        return (
+            f"{self._service_root}/home?region={self._region}#"
+            f"/layers/{name}/versions/{version}?tab=versions"
+        )
--- tests/srv/test_awslambda.py
+++ tests/srv/test_awslambda.py
@@ -5,7 +5,10 @@ from aws_console_url.tests import console, resource
 
 def test():
     func = "test"
-    lbd_func_arn = resource.LambdaFunctionArn.make(
+    layer = "test"
+
+    # --- arn
+    lbd_func_arn = resource.LambdaFunction.make(
         aws_account_id=console.aws_account_id,
         aws_region=console.aws_region,
         name=func,
@@ -14,10 +17,10 @@ def test():
     assert lbd_func_arn.is_version is False
     assert lbd_func_arn.is_alias is False
 
-    lbd_func_arn1 = resource.LambdaFunctionArn.from_arn(lbd_func_arn.arn)
+    lbd_func_arn1 = resource.LambdaFunction.from_arn(lbd_func_arn.arn)
     assert lbd_func_arn == lbd_func_arn1
 
-    lbd_func_arn = resource.LambdaFunctionArn.make(
+    lbd_func_arn = resource.LambdaFunction.make(
         aws_account_id=console.aws_account_id,
         aws_region=console.aws_region,
         name=func,
@@ -28,7 +31,7 @@ def test():
     assert lbd_func_arn.is_alias is False
     assert lbd_func_arn.arn == "arn:aws:lambda:us-east-1:669508176277:function:test:1"
 
-    lbd_func_arn = resource.LambdaFunctionArn.make(
+    lbd_func_arn = resource.LambdaFunction.make(
         aws_account_id=console.aws_account_id,
         aws_region=console.aws_region,
         name=func,
@@ -39,6 +42,14 @@ def test():
     assert lbd_func_arn.is_alias is True
     assert lbd_func_arn.arn == "arn:aws:lambda:us-east-1:669508176277:function:test:v1"
 
+    lbd_layer = resource.LambdaLayer.from_arn(
+        resource.LambdaLayer.make(
+            console.aws_account_id, console.aws_region, layer, 1
+        ).arn
+    )
+    assert lbd_layer.name == layer
+
+    # --- console
     print(console.awslambda.functions)
     print(console.awslambda.layers)
     print(console.awslambda.filter_functions(func))
@@ -51,6 +62,8 @@ def test():
     print(console.awslambda.get_function_version_tab(func))
     print(console.awslambda.get_function_version(func, 1))
     print(console.awslambda.get_function_alias(func, "v1"))
+
+    print(console.awslambda.get_layer(layer))
 
 
 if __name__ == "__main__":
