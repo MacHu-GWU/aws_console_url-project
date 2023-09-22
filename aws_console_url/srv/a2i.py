1d6e391e062b8d5049949433eb537298c0cd6ee7
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-09-22 08:41:12 -0400

    Update a2i.py

--- aws_console_url/srv/a2i.py
+++ aws_console_url/srv/a2i.py
@@ -1,25 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import typing as T
 import dataclasses
 import aws_arns.api as aws_arns
 
 from ..model import Service
-from .sagemaker import BaseSageMakerResource
-
-
-# @dataclasses.dataclass(frozen=True)
-# class A2IFlowDefinition(BaseSageMakerResource):
-#     _RESOURCE_TYPE = "flow-definition"
-#
-#
-# @dataclasses.dataclass(frozen=True)
-# class A2IHumanTaskUI(BaseSageMakerResource):
-#     _RESOURCE_TYPE = "human-task-ui"
-#
-#
-# @dataclasses.dataclass(frozen=True)
-# class A2IHumanLoop(BaseSageMakerResource):
-#     _RESOURCE_TYPE = "human-loop"
 
 
 @dataclasses.dataclass(frozen=True)
@@ -30,60 +15,77 @@ class A2I(Service):
     @property
     def human_review_workflows(self) -> str:
         return (
-            f"{self._service_root}?region={self._region}#/a2i/human-review-workflows"
+            f"{self._service_root}?region={self._region}#"
+            f"/a2i/human-review-workflows"
         )
 
-    def get_human_review_workflow_arn(self, name: str) -> str:
-        return aws_arns.res.A2IHumanReviewWorkflow.new(self._account_id, self._region, name=name).to_arn()
+    def _get_a2i_object(
+        self,
+        name_or_arn: str,
+        class_: T.Type[
+            T.Union[
+                aws_arns.res.A2IHumanReviewWorkflow,
+                aws_arns.res.A2IWorkerTaskTemplate,
+                aws_arns.res.A2IHumanLoop,
+            ]
+        ],
+    ):
+        if name_or_arn.startswith("arn:"):
+            return class_.from_arn(name_or_arn)
+        else:
+            return class_.new(self._account_id, self._region, name_or_arn)
 
-    def _human_review_workflow_arn_to_name(self, arn: str) -> str:
-        return aws_arns.res.A2IHumanReviewWorkflow.from_arn(arn).name
+    def get_human_review_workflow_arn(self, name: str) -> str:
+        return self._get_a2i_object(name, aws_arns.res.A2IHumanReviewWorkflow).to_arn()
 
     def get_human_review_workflow(self, name_or_arn: str) -> str:
-        name = self._ensure_name(name_or_arn, self._human_review_workflow_arn_to_name)
-        return f"{self._service_root}?region={self._region}#/a2i/human-review-workflows/{name}"
+        obj = self._get_a2i_object(name_or_arn, aws_arns.res.A2IHumanReviewWorkflow)
+        return (
+            f"{self._service_root}?region={obj.aws_region}#"
+            f"/a2i/human-review-workflows/{obj.name}"
+        )
 
     # --- Worker Task Templates
     @property
     def worker_task_templates(self) -> str:
-        return f"{self._service_root}?region={self._region}#/a2i/worker-task-templates"
+        return (
+            f"{self._service_root}?region={self._region}#" f"/a2i/worker-task-templates"
+        )
 
     def get_worker_task_template_arn(self, name: str) -> str:
-        return aws_arns.res.A2IWorkerTaskTemplate.new(self._account_id, self._region, name=name).to_arn()
-
-    def _worker_task_template_arn_to_name(self, arn: str) -> str:
-        return aws_arns.res.A2IWorkerTaskTemplate.from_arn(arn).name
+        return self._get_a2i_object(name, aws_arns.res.A2IWorkerTaskTemplate).to_arn()
 
     def get_worker_task_template(self, name_or_arn: str) -> str:
-        name = self._ensure_name(name_or_arn, self._worker_task_template_arn_to_name)
-        return f"{self._service_root}?region={self._region}#/a2i/worker-task-templates/{name}"
+        obj = self._get_a2i_object(name_or_arn, aws_arns.res.A2IWorkerTaskTemplate)
+        return (
+            f"{self._service_root}?region={obj.aws_region}#"
+            f"/a2i/worker-task-templates/{obj.name}"
+        )
 
     # --- Human review workforces
     @property
     def human_review_workforces(self) -> str:
         return (
-            f"{self._service_root}?region={self._region}#/a2i/human-review-workforces"
+            f"{self._service_root}?region={self._region}#"
+            f"/a2i/human-review-workforces"
         )
 
     # --- Human Loop
     def get_human_loop_arn(self, name: str) -> str:
-        return aws_arns.res.A2IHumanLoop.new(self._account_id, self._region, name=name).to_arn()
-
-    def _human_loop_arn_to_name(self, arn: str) -> str:
-        return aws_arns.res.A2IWorkerTaskTemplate.from_arn(arn).name
+        return self._get_a2i_object(name, aws_arns.res.A2IHumanLoop).to_arn()
 
     def get_human_loop(
         self,
         flow_name_or_arn: str,
         human_loop_name_or_arn: str,
     ) -> str:
-        flow_name = self._ensure_name(
-            flow_name_or_arn, self._human_review_workflow_arn_to_name
+        flow_def = self._get_a2i_object(
+            flow_name_or_arn, aws_arns.res.A2IHumanReviewWorkflow
         )
-        human_loop_name = self._ensure_name(
-            human_loop_name_or_arn, self._human_loop_arn_to_name
+        human_loop = self._get_a2i_object(
+            human_loop_name_or_arn, aws_arns.res.A2IHumanLoop
         )
         return (
-            f"{self._service_root}?region={self._region}#/a2i/human-review-workflows"
-            f"/{flow_name}/human-loops/{human_loop_name}"
+            f"{self._service_root}?region={flow_def.aws_region}#/a2i/human-review-workflows"
+            f"/{flow_def.name}/human-loops/{human_loop.name}"
         )
