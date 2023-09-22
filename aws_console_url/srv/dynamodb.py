1647ee13dd057268d9ab047ad0f32dcf70728e7f
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-09-22 12:20:58 -0400

    refact dynamodb

--- aws_console_url/srv/dynamodb.py
+++ aws_console_url/srv/dynamodb.py
@@ -3,13 +3,9 @@
 import typing as T
 import dataclasses
 
-from ..model import Resource, BaseServiceResourceV1, Service
+import aws_arns.api as aws_arns
 
-
-@dataclasses.dataclass(frozen=True)
-class DynamoDBTable(BaseServiceResourceV1):
-    _SERVICE_NAME = "dynamodb"
-    _RESOURCE_TYPE = "table"
+from ..model import Service
 
 
 @dataclasses.dataclass(frozen=True)
@@ -17,11 +13,21 @@ class Dynamodb(Service):
     _AWS_SERVICE = "dynamodbv2"
 
     # --- arn
+    def _get_table_obj(self, name_or_arn: str) -> aws_arns.res.DynamodbTable:
+        if name_or_arn.startswith("arn:"):
+            return aws_arns.res.DynamodbTable.from_arn(name_or_arn)
+        else:
+            return aws_arns.res.DynamodbTable.new(
+                self._account_id,
+                self._region,
+                name_or_arn,
+            )
+
     def get_table_arn(self, name: str) -> str:
         """
         Get DynamoDB table ARN.
         """
-        return DynamoDBTable.make(self._account_id, self._region, name).arn
+        return self._get_table_obj(name).to_arn()
 
     # --- dashboard
     @property
@@ -29,30 +35,61 @@ class Dynamodb(Service):
         return f"{self._service_root}/home?region={self._region}#tables"
 
     # --- table
-    def get_table_overview(self, table: str) -> str:
+    def _get_table_tab(
+        self,
+        table_or_arn: str,
+        tab: str,
+    ) -> str:
+        obj = self._get_table_obj(table_or_arn)
         return (
-            f"{self._service_root}/home?region={self._region}"
-            f"#table?initialTagKey=&name={table}&tab=overview"
+            f"{self._service_root}/home?region={obj.aws_region}"
+            f"#table?initialTagKey=&name={obj.table_name}&tab={tab}"
         )
 
-    def get_table_items(self, table: str) -> str:
+    def get_table(self, table_or_arn: str) -> str:
+        return self._get_table_tab(table_or_arn, "overview")
+
+    def get_table_overview(self, table_or_arn: str) -> str:
+        return self._get_table_tab(table_or_arn, "overview")
+
+    def get_table_indexes(self, table_or_arn: str) -> str:
+        return self._get_table_tab(table_or_arn, "indexes")
+
+    def get_table_monitoring(self, table_or_arn: str) -> str:
+        return self._get_table_tab(table_or_arn, "monitoring")
+
+    def get_table_global_tables(self, table_or_arn: str) -> str:
+        return self._get_table_tab(table_or_arn, "globalTables")
+
+    def get_table_backups(self, table_or_arn: str) -> str:
+        return self._get_table_tab(table_or_arn, "backups")
+
+    def get_table_exports_and_streams(self, table_or_arn: str) -> str:
+        return self._get_table_tab(table_or_arn, "streams")
+
+    def get_table_additional_settings(self, table_or_arn: str) -> str:
+        return self._get_table_tab(table_or_arn, "settings")
+
+    def get_table_items(self, table_or_arn: str) -> str:
+        obj = self._get_table_obj(table_or_arn)
         return (
-            f"{self._service_root}/home?region={self._region}"
-            f"#item-explorer?initialTagKey=&maximize=true&table={table}"
+            f"{self._service_root}/home?region={obj.aws_region}"
+            f"#item-explorer?initialTagKey=&maximize=true&table={obj.table_name}"
         )
 
     def get_item_details(
         self,
-        table: str,
+        table_or_arn: str,
         hash_key: T.Any,
         range_key: T.Optional[T.Any] = None,
     ) -> str:
+        obj = self._get_table_obj(table_or_arn)
         if range_key is None:
             range_key_param = "sk"
         else:
             range_key_param = f"sk={range_key}"
         return (
-            f"{self._service_root}/home?region={self._region}"
-            f"#edit-item?table={table}"
+            f"{self._service_root}/home?region={obj.aws_region}"
+            f"#edit-item?table={obj.table_name}"
             f"&itemMode=2&pk={hash_key}&{range_key_param}&route=ROUTE_ITEM_EXPLORER"
         )
--- tests/srv/test_dynamodb.py
+++ tests/srv/test_dynamodb.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from aws_console_url.tests import resource, console, prefix_snake
+from aws_console_url.tests import console, prefix_snake
 
 
 def test():
@@ -11,16 +11,25 @@ def test():
     table1_arn = console.dynamodb.get_table_arn(table1)
     table2_arn = console.dynamodb.get_table_arn(table2)
 
-    assert resource.DynamoDBTable.from_arn(table1_arn).arn == table1_arn
-    assert resource.DynamoDBTable.from_arn(table2_arn).arn == table2_arn
-
     # --- console
     print(console.dynamodb.tables)
+
+    print(console.dynamodb.get_table(table1))
+    print(console.dynamodb.get_table(table1_arn))
     print(console.dynamodb.get_table_overview(table1))
+    print(console.dynamodb.get_table_indexes(table1))
+    print(console.dynamodb.get_table_monitoring(table1))
+    print(console.dynamodb.get_table_global_tables(table1))
+    print(console.dynamodb.get_table_backups(table1))
+    print(console.dynamodb.get_table_exports_and_streams(table1))
+    print(console.dynamodb.get_table_additional_settings(table1))
     print(console.dynamodb.get_table_items(table1))
+    print(console.dynamodb.get_table_items(table1_arn))
 
     print(console.dynamodb.get_item_details(table1, "pk-1"))
+    print(console.dynamodb.get_item_details(table1_arn, "pk-1"))
     print(console.dynamodb.get_item_details(table2, "pk-1", "sk-1"))
+    print(console.dynamodb.get_item_details(table2_arn, "pk-1", "sk-1"))
 
 
 if __name__ == "__main__":
