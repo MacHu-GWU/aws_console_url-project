5779826ff0fac4a131cfcdef4c8bf76cd75f7344
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-09-22 22:46:56 -0400

    fix ec2

--- aws_console_url/srv/ec2.py
+++ aws_console_url/srv/ec2.py
@@ -3,6 +3,8 @@
 import typing as T
 import dataclasses
 
+import aws_arns.api as aws_arns
+
 from ..model import Service
 
 
@@ -11,11 +13,28 @@ class EC2(Service):
     _AWS_SERVICE = "ec2"
 
     # --- arn
+    def _get_ec2_obj(
+        self,
+        id_or_arn: str,
+        class_,
+    ):
+        if id_or_arn.startswith("arn:"):
+            return class_.from_arn(id_or_arn)
+        else:
+            return class_.new(
+                self._account_id,
+                self._region,
+                id_or_arn,
+            )
 
     # --- dashboard
     @property
     def instances(self) -> str:
-        return f"{self._service_root}/home?region={self._region}#Instances:"
+        return f"{self._service_root}/home?region={self._region}" f"#Instances:"
+
+    @property
+    def launch_templates(self) -> str:
+        return f"{self._service_root}/home?region={self._region}" f"#LaunchTemplates:"
 
     @property
     def amis(self) -> str:
@@ -23,6 +42,22 @@ class EC2(Service):
             f"{self._service_root}/home?region={self._region}"
             "#Images:visibility=owned-by-me"
         )
+
+    @property
+    def volumes(self) -> str:
+        return f"{self._service_root}/home?region={self._region}#Volumes:"
+
+    @property
+    def snapshots(self) -> str:
+        return f"{self._service_root}/home?region={self._region}#Snapshots:"
+
+    @property
+    def eips(self) -> str:
+        return f"{self._service_root}/home?region={self._region}#Addresses:"
+
+    @property
+    def keys(self) -> str:
+        return f"{self._service_root}/home?region={self._region}#KeyPairs:"
 
     # --- instance
     def filter_instances_by_name(self, facets: T.Union[str, T.List[str]]) -> str:
@@ -36,10 +71,14 @@ class EC2(Service):
             f"#Instances:search={search};v=3;$case=tags:true%5C,client:false;$regex=tags:false%5C,client:false"
         )
 
-    def get_instance(self, instance_id: str) -> str:
+    def get_instance(
+        self,
+        instance_id_or_arn: str,
+    ) -> str:
+        obj = self._get_ec2_obj(instance_id_or_arn, aws_arns.res.Ec2Instance)
         return (
-            f"{self._service_root}/home?region={self._region}"
-            f"#InstanceDetails:instanceId={instance_id}"
+            f"{self._service_root}/home?region={obj.aws_region}"
+            f"#InstanceDetails:instanceId={obj.resource_id}"
         )
 
     # --- ami
@@ -54,8 +93,78 @@ class EC2(Service):
             f"#Images:visibility=owned-by-me;search={search};v=3;$case=tags:false%5C,client:false;$regex=tags:false%5C,client:false"
         )
 
-    def get_ami(self, image_id: str) -> str:
+    def get_ami(
+        self,
+        image_id_or_arn: str,
+    ) -> str:
+        if image_id_or_arn.startswith("arn:"):
+            obj = aws_arns.res.Ec2Image.from_arn(image_id_or_arn)
+        else:
+            obj = aws_arns.res.Ec2Image.new(self._region, image_id_or_arn)
+        return (
+            f"{self._service_root}/home?region={obj.aws_region}"
+            f"#ImageDetails:imageId={obj.ami_id}"
+        )
+
+    def filter_volumes_by_name(self, facets: T.Union[str, T.List[str]]) -> str:
+        if isinstance(facets, str):
+            facets = [
+                facets,
+            ]
+        search = ",".join([f":{facet}" for facet in facets])
         return (
             f"{self._service_root}/home?region={self._region}"
-            f"#ImageDetails:imageId={image_id}"
+            f"#Volumes:v=3;search={search}"
+        )
+
+    def get_volume(
+        self,
+        volume_id_or_arn: str,
+    ) -> str:
+        obj = self._get_ec2_obj(volume_id_or_arn, aws_arns.res.EbsVolume)
+        return (
+            f"{self._service_root}/home?region={obj.aws_region}"
+            f"#VolumeDetails:volumeId={obj.resource_id}"
+        )
+
+    def filter_snapshotss_by_name(self, facets: T.Union[str, T.List[str]]) -> str:
+        if isinstance(facets, str):
+            facets = [
+                facets,
+            ]
+        search = ",".join([f":{facet}" for facet in facets])
+        return (
+            f"{self._service_root}/home?region={self._region}"
+            f"#Snapshots:visibility=owned-by-me;v=3;search={search}"
+        )
+
+    def get_snapshot(
+        self,
+        snapshot_id_or_arn: str,
+    ) -> str:
+        obj = self._get_ec2_obj(snapshot_id_or_arn, aws_arns.res.EbsSnapshot)
+        return (
+            f"{self._service_root}/home?region={obj.aws_region}"
+            f"#SnapshotDetails:snapshotId={obj.resource_id}"
+        )
+
+    def filter_eip_by_name(self, facets: T.Union[str, T.List[str]]) -> str:
+        if isinstance(facets, str):
+            facets = [
+                facets,
+            ]
+        search = ",".join(facets)
+        return (
+            f"{self._service_root}/home?region={self._region}"
+            f"#Addresses:search={search}"
+        )
+
+    def get_eip(
+        self,
+        allocation_id_or_arn: str,
+    ) -> str:
+        obj = self._get_ec2_obj(allocation_id_or_arn, aws_arns.res.ElasticIpAllocation)
+        return (
+            f"{self._service_root}/home?region={obj.aws_region}"
+            f"#ElasticIpDetails:AllocationId={obj.resource_id}"
         )
--- tests/srv/test_ec2.py
+++ tests/srv/test_ec2.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from aws_console_url.tests import resource, console
+from aws_console_url.tests import console, prefix_snake
 
 
 def test():
@@ -10,16 +10,31 @@ def test():
     image_name = "wserver-build-env-with-python-2022-09-06"
     image_id = "ami-081755a1bae54d330"
 
-    # --- resource
+    vol_id = "vol-036bad5ae240736c2"
+    snap_id = "snap-0eab54c73db1931f1"
+    eip_id = "eipalloc-0c046c3022bc1d3c8"
 
     # --- console
+    print("-" * 80)
     print(console.ec2.instances)
+    print(console.ec2.launch_templates)
     print(console.ec2.amis)
+    print(console.ec2.volumes)
+    print(console.ec2.snapshots)
+    print(console.ec2.eips)
+    print(console.ec2.keys)
 
+    print("-" * 80)
     print(console.ec2.filter_instances_by_name(inst_name))
     print(console.ec2.get_instance(inst_id))
     print(console.ec2.filter_amis_by_name(image_name))
     print(console.ec2.get_ami(image_id))
+    print(console.ec2.filter_volumes_by_name("vol"))
+    print(console.ec2.get_volume(vol_id))
+    print(console.ec2.filter_snapshotss_by_name("snap"))
+    print(console.ec2.get_snapshot(snap_id))
+    print(console.ec2.filter_eip_by_name("eipalloc"))
+    print(console.ec2.get_eip(eip_id))
 
 
 if __name__ == "__main__":
